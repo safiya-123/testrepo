6e49789bec3bc8a61f7f4042d485b2a8d4ae11b4
safiya-123 <144349887+safiya-123@users.noreply.github.com>
2023-09-12 07:32:33 +0530

    Update JwtTokenService.cs

--- JwtTokenService.cs
+++ JwtTokenService.cs
@@ -1,1 +1,46 @@
-j
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using backend.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace backend.services
+{
+    public class JwtTokenService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(EmployeeMaster employee)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim("EmployeeId", employee.EmployeeId), // Use a unique identifier for the user
+            new Claim("EmployeeName", employee.EmployeeName),
+            new Claim("Designation", employee.Designation),
+            new Claim("Department", employee.Department),
+            new Claim("Gender", employee.Gender),
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(Convert.ToInt32(_configuration["Jwt:ExpireHours"])),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+
+}
+
